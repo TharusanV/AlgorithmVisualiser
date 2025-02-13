75c25ab0c688f82f3e9b2e6d525b25006de7d18c
TharusanV <tharusan.vijayakumar@gmail.com>
2025-02-13 10:39:17 +0000

    Basic project structure defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 
+import { Home } from "./components";
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </HashRouter>
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Home from "./Home.jsx";
+import Arrays from "./Arrays.jsx";
+import LinkedLists from "./LinkedLists.jsx";
+import Stack from "./Stack.jsx";
+import Queue from "./Queue.jsx";
+
+export{
+  Home,
+  Arrays,
+  LinkedLists,
+  Stack,
+  Queue,
+
+}
